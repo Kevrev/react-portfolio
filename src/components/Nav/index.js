db7663069f09a976a61be6191a86cd83b1ec1e64
Seer <ksmnk@protonmail.com>
2023-07-19 23:16:12 -0700

    added navbar test (properly routed) and additional packages for development

---
+++ src/components/Nav/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import './index.css';
+
+function Nav() {
+    return (
+        <nav>
+        <ul className="nav">
+            <li className="nav-item">
+                test
+            {/* <Link className="nav-link active" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+            <Link className="nav-link active" to="/about">About</Link> */}
+            </li>
+        </ul>
+        </nav>
+    );
+}
+
+export default Nav;
