c7be27fda1aad8a92eb33d4e9e50111ce84eb285
Seer <ksmnk@protonmail.com>
2023-07-20 14:58:07 -0700

    added props to project cards so that projects data can be displayed

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,44 +1,74 @@
-import React from 'react';
-import ActionAreaCard from '../components/Project';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import './Portfolio.css';
+import React from "react";
+import ActionAreaCard from "../components/Project";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import "./Portfolio.css";
 
 function Portfolio() {
-    return (
-      <>
-        <div className='contentContainer'>
-            <h2  className="portfolioTitle">Portfolio</h2>
-            <hr style={{margin: '5px 0 20px 0'}}/>
-            <div className="projectsContainer">
-                <Box sx={{ flexGrow: 1 }}>
-                  <Grid container spacing={5}>
-                    {/* First Row */}
-                    <Grid item xs={4}>
-                      <ActionAreaCard />
-                    </Grid>
-                    <Grid item xs={4}>
-                      <ActionAreaCard />
-                    </Grid>
-                    <Grid item xs={4}>
-                      <ActionAreaCard />
-                    </Grid>
-                    {/* Second Row */}
-                    <Grid item xs={4}>
-                      <ActionAreaCard />
-                    </Grid>
-                    <Grid item xs={4}>
-                      <ActionAreaCard />
-                    </Grid>
-                    <Grid item xs={4}>
-                      <ActionAreaCard />
-                    </Grid>
-                  </Grid>
-                </Box>
-            </div>
+  return (
+    <>
+      <div className="contentContainer">
+        <h2 className="portfolioTitle">Portfolio</h2>
+        <hr style={{ margin: "5px 0 20px 0" }} />
+        <div className="projectsContainer">
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={5}>
+              {/* First Row */}
+              <Grid item xs={4}>
+                <ActionAreaCard
+                  title="Lizard"
+                  description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica."
+                  imageUrl="/static/images/cards/contemplative-reptile.jpg"
+                  altText="Lizard"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <ActionAreaCard
+                  title="Lizard"
+                  description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica."
+                  imageUrl="/static/images/cards/contemplative-reptile.jpg"
+                  altText="Lizard"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <ActionAreaCard
+                  title="Lizard"
+                  description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica."
+                  imageUrl="/static/images/cards/contemplative-reptile.jpg"
+                  altText="Lizard"
+                />
+              </Grid>
+              {/* Second Row */}
+              <Grid item xs={4}>
+                <ActionAreaCard
+                  title="Lizard"
+                  description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica."
+                  imageUrl="/static/images/cards/contemplative-reptile.jpg"
+                  altText="Lizard"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <ActionAreaCard
+                  title="Lizard"
+                  description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica."
+                  imageUrl="/static/images/cards/contemplative-reptile.jpg"
+                  altText="Lizard"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <ActionAreaCard
+                  title="Lizard"
+                  description="Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica."
+                  imageUrl="/static/images/cards/contemplative-reptile.jpg"
+                  altText="Lizard"
+                />
+              </Grid>
+            </Grid>
+          </Box>
         </div>
-      </>
-    );
-  }
+      </div>
+    </>
+  );
+}
 
 export default Portfolio;
