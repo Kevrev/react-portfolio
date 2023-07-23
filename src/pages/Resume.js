7a35c967e01d3edbde99cbd3029a2c166217635b
Seer <ksmnk@protonmail.com>
2023-07-22 21:03:05 -0700

    added resume (temp)

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,36 +1,14 @@
 import React from "react";
-import "./Resume.css";
+import resume from "../assets/docs/KevinSmallResume.pdf";
 
 function Resume() {
   return (
     <>
       <div className="contentContainer">
-        <h2 className="contentTitle">About Me</h2>
+        <h2 className="contentTitle">Resume</h2>
         <hr style={{ margin: "5px 0 20px 0" }} />
         <div className="aboutContainer">
-          <div className="aboutTextContainer">
-            <p>
-              Hello and welcome to my portfolio! My name is Kevin Small and I am
-              a full stack web developer. I am a recent graduate of the
-              University of Berkley Extension's Fullstack Coding Bootcamp. I
-              have experience with HTML, CSS, JavaScript, Node.js, Express.js,
-              MySQL, MongoDB, GraphQL, and React.js. I'm always learning new
-              things and enjoy exploring new technologies to solve the problems
-              we face in the development world.
-            </p>
-            <p>
-              Outside of development, I really enjoy activites like rock
-              climbing and hiking. It's a great way to get out into nature and
-              away from the computer screen! I also have a deep passion for art
-              but as I've begun my coding journey, I've also found myself really
-              enjoying design and thinking about the UI/UX of the applications
-              I'm building. It's vital for applications to be easy to use and
-              accessible to all users. One thing I'd really like to do in the
-              future is design and development my own component library. Shoot
-              me an email over at <b>ksmnk@protonmail.com</b> if you'd like to
-              get in touch!
-            </p>
-          </div>
+          <embed src={resume} type="application/pdf" width="100%" height="800px" />
         </div>
       </div>
     </>
