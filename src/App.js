5cbe59d82aa3877bc7cbaa2628cfa72cd6a215cc
Seer <ksmnk@protonmail.com>
2023-07-22 21:37:18 -0700

    finalizing for submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
 import Contact from './pages/Contact';
@@ -17,9 +17,8 @@ function App() {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/resume" element={<Resume />} />
         <Route path="/contact" element={<Contact />} />
-        {/* Always land on about page, even with 404 */}
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<About />} />
+        <Route path="/" element={<About />} />
       </Routes>
     </Router>
   );
